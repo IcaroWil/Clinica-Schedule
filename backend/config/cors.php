1b2c48559f19fd990d802fb9c0a660973e8d8e48
IcaroWil <icarofaria11@gmail.com>
2025-09-24 13:22:30 -0300

    Add CORS configuration file for API
    - Created a new cors.php file to define cross-origin resource sharing settings, allowing requests from specified origins and enabling all methods and headers for API endpoints.

---
+++ backend/config/cors.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    'paths' => ['api/*', 'oauth/*', 'sanctum/csrf-cookie'],
+
+    'allowed_methods' => ['*'],
+
+    'allowed_origins' => ['https://*.vercel.app'],
+
+    'allowed_origins_patterns' => [],
+
+    'allowed_headers' => ['*'],
+
+    'exposed_headers' => [],
+
+    'max_age' => 0,
+
+    'supports_credentials' => false,
+];
