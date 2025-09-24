7ccb07b6d0040a4ab6a5e88622b8ab025fece052
IcaroWil <icarofaria11@gmail.com>
2025-09-24 13:22:37 -0300

    Enhance Docker entrypoint script for application setup
    - Added commands to generate OAuth keys if not present, create a symbolic link for storage, and cache configuration and routes to optimize application performance during initialization.

--- backend/docker/entrypoint.sh
+++ backend/docker/entrypoint.sh
@@ -8,4 +8,10 @@ chmod -R 777 storage bootstrap/cache || true
 
 php artisan migrate --force --no-interaction || true
 
+[ -f storage/oauth-private.key ] || php artisan passport:keys --force || true
+
+php artisan storage:link || true
+php artisan config:cache
+php artisan route:cache
+
 exec php -d variables_order=EGPCS -S 0.0.0.0:${PORT} -t public public/index.php
