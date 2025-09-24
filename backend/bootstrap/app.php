ae8a8fa827e5d5ed9a29a5d50b27d82e49b1d6a1
IcaroWil <icarofaria11@gmail.com>
2025-09-24 13:22:25 -0300

    Add CORS middleware to API group in app bootstrap
    - Appended HandleCors middleware to the API group in the application bootstrap file to enhance cross-origin resource sharing capabilities.

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -12,7 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->appendToGroup('api', HandleCors::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
